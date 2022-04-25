2cf4ec9946b8c11eb6de2860a228c5aed8c12039
swapnamk2618 <swapnakohrekar@gmail.com>
2022-04-25 17:51:09 +0530

    Added a class demo,and write addnumber logic

---
+++ src/main/java/com/dlithe/bankingapp/practice/Demo.java
@@ -0,0 +1,18 @@
+/**
+ * This class is copyright property of DLithe organization
+ */
+package com.dlithe.bankingapp.practice;
+
+/**
+ * @author Swapna Kochrekar
+ * @version 1.0
+ * @since
+ *
+ */
+public class Demo {
+    public void addNumbers(int a,int b){
+        a=10;
+        b=5;
+        System.out.println(a+b);
+    }
+}
